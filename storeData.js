df24b99aa66651a2a5e7b4c8ee2ce058caaa8402
Jonathan Easterman <jonathaneasterman@gmail.com>
2021-09-29 15:13:53 +0100

    Store USD versions of metrics

--- storeData.js
+++ storeData.js
@@ -14,21 +14,34 @@ function timestamp(){
     return Math.round(Date.now()/1000)
 }
 
+async function getEthPrice() {
+    const coingeckoEndpoint = 'https://api.coingecko.com/api/v3/coins/ethereum'
+
+    const response = await axios.get(coingeckoEndpoint)
+    const { usd } = response.data.market_data.current_price
+    return usd
+}
+
 async function getNFTDataAndStore() {
     while (true) {
         for (const collection of collections) {
             const url = `https://api.opensea.io/api/v1/asset/${collection.contract}/${collection.item}/`
             const data = await axios.get(url)
-            const { one_day_volume, num_owners, floor_price, total_volume } = data.data.collection.stats
+            const { one_day_volume, num_owners, floor_price, total_volume, total_supply } = data.data.collection.stats
+            const ethInUsd = await getEthPrice()
             await ddb.put({
                 TableName,
                 'Item': {
                     PK: `nfts#${collection.id}`,
                     SK: timestamp(),
                     dailyVolume: one_day_volume,
+                    dailyVolumeUsd: one_day_volume * ethInUsd,
                     owners: num_owners,
                     floor: floor_price,
-                    totalVolume: total_volume
+                    floorUsd: floor_price * ethInUsd,
+                    totalVolume: total_volume,
+                    totalVolumeUsd: total_volume * ethInUsd,
+                    marketCap: floor_price * ethInUsd * total_supply,
                 },
             }).promise();
             await sleep(30)
@@ -38,5 +51,6 @@ async function getNFTDataAndStore() {
 }
 
 module.exports = {
-    getNFTDataAndStore
+    getNFTDataAndStore,
+    getEthPrice,
 }
