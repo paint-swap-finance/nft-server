d6ee4867524cb74fec823313c48953fd89b1d2b8
degendevv <degendevv@protonmail.com>
2021-10-14 14:37:51 +0100

    Add shared types and util functions

--- src/utils.ts
+++ src/utils.ts
@@ -1,1 +1,9 @@
 export const sleep = async (seconds: number): Promise<void> => new Promise(resolve => setTimeout(resolve, seconds * 1000))
+
+export function timestamp(): number {
+  return Math.round(Date.now()/1000)
+}
+
+export function roundUSD(num: number): number {
+  return Math.round(num)
+}
