052c80d74eab37777264277d7831cba6cb06c6d6
jpegbodhi <92826208+jpegbodhi@users.noreply.github.com>
2021-11-23 10:15:04 +0800

    Retrieve SOL price from Coingecko

--- src/api/coingecko.ts
+++ src/api/coingecko.ts
@@ -1,10 +1,17 @@
 import axios from "axios";
 
 export class Coingecko {
-  private static endpoint = "https://api.coingecko.com/api/v3/coins/ethereum";
+  private static ETH_ENDPOINT = "https://api.coingecko.com/api/v3/coins/ethereum";
+  private static SOL_ENDPOINT = "https://api.coingecko.com/api/v3/coins/solana";
 
   public static async getEthPrice(): Promise<number> {
-    const response = await axios.get(Coingecko.endpoint);
+    const response = await axios.get(Coingecko.ETH_ENDPOINT);
+    const { usd } = response.data.market_data.current_price;
+    return usd;
+  }
+
+  public static async getSolPrice(): Promise<number> {
+    const response = await axios.get(Coingecko.SOL_ENDPOINT);
     const { usd } = response.data.market_data.current_price;
     return usd;
   }
