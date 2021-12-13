2b3a52d8932b0ad9ce8dc508bc71f3bbd4e13936
jpegbodhi <92826208+jpegbodhi@users.noreply.github.com>
2021-12-13 15:16:37 +0800

    Add getMarketplaces endpoint

---
+++ src/handlers/getMarketplaces.ts
@@ -0,0 +1,39 @@
+import { getGlobalStatistics } from "../utils/dynamodb";
+import {
+  successResponse,
+  errorResponse,
+  IResponse,
+} from "../utils/lambda-response";
+import { Marketplace } from "../types";
+
+const handler = async (event: any): Promise<IResponse> => {
+  try {
+    const globalStatistics = await getGlobalStatistics();
+    const marketplaces = Object.entries(Marketplace).map((marketplace) => ({
+      displayName: marketplace[0],
+      marketplace: marketplace[1],
+    }));
+    let marketplacesData = [];
+
+    for (const marketplace of marketplaces) {
+      marketplacesData.push({
+        ...marketplace,
+        dailyVolumeUSD:
+          globalStatistics[globalStatistics.length - 1][
+            `marketplace_${marketplace.marketplace}_volumeUSD`
+          ],
+        totalVolumeUSD: globalStatistics.reduce((volume, entry) => {
+          return (
+            volume + entry[`marketplace_${marketplace.marketplace}_volumeUSD`]
+          );
+        }, 0),
+      });
+    }
+    return successResponse(marketplacesData);
+  } catch (e) {
+    console.log(e);
+    return errorResponse({ message: "Error" });
+  }
+};
+
+export default handler;
