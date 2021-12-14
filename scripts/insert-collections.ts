7bd6e1a5567341fd009d572d68bf55e4dcd78283
jpegbodhi <92826208+jpegbodhi@users.noreply.github.com>
2021-12-14 08:24:29 +0800

    Update insert collections script

--- scripts/insert-collections.ts
+++ scripts/insert-collections.ts
@@ -1,46 +1,41 @@
-import { createConnection } from "typeorm";
-import { Sale } from "../src/models/sale";
-import { Collection } from "../src/models/collection";
-import { Statistic } from "../src/models/statistic";
-import { HistoricalStatistic } from "../src/models/historical-statistic";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "../env";
-import { Blockchain } from "../src/types";
+import { Blockchain, Marketplace } from "../src/types";
+import { upsertCollection } from "../src/utils/dynamodb";
 
 const collections = [
   {
     address: "",
     slug: "",
-    chain: Blockchain.Any,
+    chain: Blockchain.Arbitrum,
+    marketplace: Marketplace.Treasure,
     name: "",
     symbol: "",
     description: "",
-    defaultTokenId: "",
     logo: "",
     website: "",
+    totalVolumeUSD: 0,
   },
 ];
 
-const main = () => {
-  createConnection({
-    type: "postgres",
-    host: DB_HOST,
-    port: DB_PORT,
-    username: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    entities: [Sale, Collection, Statistic, HistoricalStatistic],
-    synchronize: true,
-    logging: false,
-  })
-    .then(async (connection) => {
-      console.log("Manually inserting", collections.length, "collections");
-      for (const collection of collections) {
-        console.log("Inserting collection", collection.name);
-        const storedCollection = await Collection.create({ ...collection });
-        await storedCollection.save();
-      }
-    })
-    .catch((e) => console.error("Error", e.message));
+const main = async () => {
+  try {
+    console.log("Manually inserting", collections.length, "collections");
+    for (const collection of collections) {
+      console.log("Inserting collection", collection.name);
+      const { slug, chain, marketplace, totalVolumeUSD, ...metadata } =
+        collection;
+      await upsertCollection({
+        slug,
+        metadata,
+        chain,
+        marketplace,
+        statistics: { totalVolumeUSD },
+      });
+    }
+    return "Successfully inserted collections";
+  } catch (e) {
+    console.log(e.message);
+    return "Error inserting collections";
+  }
 };
 
-main();
+main().then((result) => console.log(result));
