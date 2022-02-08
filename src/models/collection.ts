d9b4ad6279deb5811b0cd7de8ecad0ffd5194ccb
jpegbodhi <92826208+jpegbodhi@users.noreply.github.com>
2022-02-09 00:40:48 +0800

    Refactor collection upsert to handle multiple chains/marketplaces

--- src/models/collection.ts
+++ src/models/collection.ts
@@ -1,4 +1,10 @@
-import { Blockchain, Marketplace } from "../types";
+import { CHAIN_MARKETPLACES } from "../constants";
+import {
+  Blockchain,
+  CollectionData,
+  Marketplace,
+  StatisticData,
+} from "../types";
 import { getSlugFromPK, handleError } from "../utils";
 import dynamodb from "../utils/dynamodb";
 
@@ -16,6 +22,309 @@ export class Collection {
   twitterUsername: string;
   mediumUsername: string;
 
+  static async insert({
+    slug,
+    chain,
+    marketplace,
+    metadata,
+    statistics,
+  }: {
+    slug: string;
+    chain: Blockchain;
+    marketplace: Marketplace;
+    metadata: CollectionData;
+    statistics: StatisticData;
+  }) {
+    try {
+      const currentTime = Date.now();
+
+      const collectionData = {
+        ...metadata,
+        ...statistics,
+        createdAt: currentTime,
+        updatedAt: currentTime,
+      };
+
+      await dynamodb.transactWrite({
+        updateItems: [
+          {
+            Key: {
+              PK: `collectionCount`,
+              SK: `chain#${chain}`,
+            },
+            UpdateExpression: "ADD collections :no",
+            ExpressionAttributeValues: {
+              ":no": 1,
+            },
+          },
+          {
+            Key: {
+              PK: `collectionCount`,
+              SK: `marketplace#${marketplace}`,
+            },
+            UpdateExpression: "ADD collections :no",
+            ExpressionAttributeValues: {
+              ":no": 1,
+            },
+          },
+        ],
+        putItems: [
+          {
+            PK: `collection#${slug}`,
+            SK: "overview",
+            category: "collections",
+            ...collectionData,
+          },
+          {
+            PK: `collection#${slug}`,
+            SK: `chain#${chain}`,
+            category: `collections#chain#${chain}`,
+            ...collectionData,
+          },
+          {
+            PK: `collection#${slug}`,
+            SK: `marketplace#${marketplace}`,
+            category: `collections#marketplace#${marketplace}`,
+            ...collectionData,
+          },
+        ],
+      });
+    } catch (e) {
+      handleError(e, "collection-model:insert");
+    }
+  }
+
+  static async update({
+    slug,
+    collection,
+    statistics,
+    chain,
+    marketplace,
+  }: {
+    slug: string;
+    collection: any;
+    statistics: StatisticData;
+    chain: Blockchain;
+    marketplace: Marketplace;
+  }) {
+    const currentTime = Date.now();
+
+    // Set marketplace attribute values
+    const marketplaceAttributeValues = {
+      ":owners": statistics.owners,
+      ":totalVolume": statistics.totalVolume,
+      ":totalVolumeUSD": statistics.totalVolumeUSD,
+      ":dailyVolume": statistics.dailyVolume,
+      ":dailyVolumeUSD": statistics.dailyVolumeUSD,
+      ":floor": statistics.floor,
+      ":floorUSD": statistics.floorUSD,
+      ":marketCap": statistics.marketCap,
+      ":marketCapUSD": statistics.marketCapUSD,
+      ":updatedAt": currentTime,
+    };
+
+    // Calculate chain data
+    let chainAttributeValues = {};
+
+    const existingChains = collection
+      .filter((item: any) => item.SK.startsWith("chain"))
+      .map((item: any) => item.split("#")[1]);
+
+    if (!existingChains.includes(chain)) {
+      // Initialize chain attribute values with marketplace data
+      chainAttributeValues = marketplaceAttributeValues;
+    } else {
+      const marketplaces = CHAIN_MARKETPLACES[chain];
+      const chainData = collection.reduce(
+        (totals: any, item: any) => {
+          if (
+            marketplaces.includes(item.SK) &&
+            item.SK !== `marketplace#${marketplace}`
+          ) {
+            const {
+              ownersArr,
+              totalVolume,
+              totalVolumeUSD,
+              dailyVolume,
+              dailyVolumeUSD,
+              floorArr,
+              floorUSDArr,
+              marketCapArr,
+              marketCapUSDArr,
+            } = totals;
+
+            return {
+              totalVolume: totalVolume + item.totalVolume,
+              totalVolumeUSD: totalVolumeUSD + item.totalVolumeUSD,
+              dailyVolume: dailyVolume + item.dailyVolume,
+              dailyVolumeUSD: dailyVolumeUSD + item.dailyVolumeUSD,
+              ownersArr: item.owners ? [...ownersArr, item.owners] : ownersArr,
+              floorArr: item.floor ? [...floorArr, item.floor] : floorArr,
+              floorUSDArr: item.floorUSD
+                ? [...floorUSDArr, item.floorUSD]
+                : floorUSDArr,
+              marketCapArr: item.marketCap
+                ? [...marketCapArr, item.marketCap]
+                : marketCapArr,
+              marketCapUSDArr: item.marketCapUSD
+                ? [...marketCapUSDArr, item.marketCapUSD]
+                : marketCapUSDArr,
+            };
+          }
+
+          return totals;
+        },
+        {
+          totalVolume: statistics.totalVolume,
+          totalVolumeUSD: statistics.totalVolumeUSD,
+          dailyVolume: statistics.dailyVolume,
+          dailyVolumeUSD: statistics.dailyVolumeUSD,
+          ownersArr: [statistics.owners],
+          floorArr: [statistics.floor],
+          floorUSDArr: [statistics.floorUSD],
+          marketCapArr: [statistics.marketCap],
+          marketCapUSDArr: [statistics.marketCapUSD],
+        }
+      );
+
+      // Set chain attribute values
+      chainAttributeValues = {
+        ":totalVolume": chainData.totalVolume,
+        ":totalVolumeUSD": chainData.totalVolumeUSD,
+        ":dailyVolume": chainData.dailyVolume,
+        ":dailyVolumeUSD": chainData.dailyVolumeUSD,
+        ":owners": Math.max(chainData.ownersArr),
+        ":floor": chainData.floorArr.length ? Math.min(chainData.floorArr) : 0,
+        ":floorUSD": chainData.floorUSDArr.length
+          ? Math.min(chainData.floorUSDArr)
+          : 0,
+        ":marketCap": chainData.marketCapArr.length
+          ? Math.min(chainData.marketCapArr)
+          : 0,
+        ":marketCapUSD": chainData.marketCapUSDArr.length
+          ? Math.min(chainData.marketCapUSDArr)
+          : 0,
+        ":updatedAt": currentTime,
+      };
+    }
+
+    // Calculate overview data
+    const overviewData = collection.reduce(
+      (totals: any, item: any) => {
+        if (
+          item.SK.startsWith("marketplace") &&
+          item.SK !== `marketplace#${marketplace}`
+        ) {
+          const {
+            ownersArr,
+            totalVolume,
+            totalVolumeUSD,
+            dailyVolume,
+            dailyVolumeUSD,
+            floorArr,
+            floorUSDArr,
+            marketCapArr,
+            marketCapUSDArr,
+          } = totals;
+
+          return {
+            totalVolume: totalVolume + item.totalVolume,
+            totalVolumeUSD: totalVolumeUSD + item.totalVolumeUSD,
+            dailyVolume: dailyVolume + item.dailyVolume,
+            dailyVolumeUSD: dailyVolumeUSD + item.dailyVolumeUSD,
+            ownersArr: item.owners ? [...ownersArr, item.owners] : ownersArr,
+            floorArr: item.floor ? [...floorArr, item.floor] : floorArr,
+            floorUSDArr: item.floorUSD
+              ? [...floorUSDArr, item.floorUSD]
+              : floorUSDArr,
+            marketCapArr: item.marketCap
+              ? [...marketCapArr, item.marketCap]
+              : marketCapArr,
+            marketCapUSDArr: item.marketCapUSD
+              ? [...marketCapUSDArr, item.marketCapUSD]
+              : marketCapUSDArr,
+          };
+        }
+        return totals;
+      },
+      {
+        totalVolume: statistics.totalVolume,
+        totalVolumeUSD: statistics.totalVolumeUSD,
+        dailyVolume: statistics.dailyVolume,
+        dailyVolumeUSD: statistics.dailyVolumeUSD,
+        ownersArr: [statistics.owners],
+        floorArr: [statistics.floor],
+        floorUSDArr: [statistics.floorUSD],
+        marketCapArr: [statistics.marketCap],
+        marketCapUSDArr: [statistics.marketCapUSD],
+      }
+    );
+
+    // Set overview attribute values
+    const overviewAttributeValues = {
+      ":totalVolume": overviewData.totalVolume,
+      ":totalVolumeUSD": overviewData.totalVolumeUSD,
+      ":dailyVolume": overviewData.dailyVolume,
+      ":dailyVolumeUSD": overviewData.dailyVolumeUSD,
+      ":owners": Math.max(overviewData.ownersArr),
+      ":floor": overviewData.floorArr.length
+        ? Math.min(overviewData.floorArr)
+        : 0,
+      ":floorUSD": overviewData.floorUSDArr.length
+        ? Math.min(overviewData.floorUSDArr)
+        : 0,
+      ":marketCap": overviewData.marketCapArr.length
+        ? Math.min(overviewData.marketCapArr)
+        : 0,
+      ":marketCapUSD": overviewData.marketCapUSDArr.length
+        ? Math.min(overviewData.marketCapUSDArr)
+        : 0,
+      ":updatedAt": currentTime,
+    };
+
+    // Update items
+    const updateExpression = `
+    SET owners = :owners,
+        totalVolume = :totalVolume,
+        totalVolumeUSD = :totalVolumeUSD,
+        dailyVolume = :dailyVolume,
+        dailyVolumeUSD = :dailyVolumeUSD,
+        floor = :floor,
+        floorUSD = :floorUSD,
+        marketCap = :marketCap,
+        marketCapUSD = :marketCapUSD,
+        updatedAt = :updatedAt`;
+
+    await dynamodb.transactWrite({
+      updateItems: [
+        {
+          Key: {
+            PK: `collection#${slug}`,
+            SK: "overview",
+          },
+          UpdateExpression: updateExpression,
+          ExpressionAttributeValues: overviewAttributeValues,
+        },
+        {
+          Key: {
+            PK: `collection#${slug}`,
+            SK: `chain#${chain}`,
+          },
+          UpdateExpression: updateExpression,
+          ExpressionAttributeValues: chainAttributeValues,
+        },
+        {
+          Key: {
+            PK: `collection#${slug}`,
+            SK: `marketplace#${marketplace}`,
+          },
+          UpdateExpression: updateExpression,
+          ExpressionAttributeValues: marketplaceAttributeValues,
+        },
+      ],
+    });
+  }
+
   static async upsert({
     slug,
     metadata,
@@ -24,142 +333,34 @@ export class Collection {
     marketplace,
   }: {
     slug: string;
-    metadata: any;
-    statistics: any;
+    metadata: CollectionData;
+    statistics: StatisticData;
     chain: Blockchain;
     marketplace: Marketplace;
   }) {
     try {
       const existingCollection = await Collection.get(slug);
-      const currentTime = Date.now();
 
-      // If collection already exists
+      // If collection already exists, update statistics
       if (existingCollection.length) {
-        const { chains: existingChains, marketplaces: existingMarketplaces } =
-          existingCollection[0];
-
-        const chains = existingChains.includes(chain)
-          ? existingChains
-          : [...existingChains, chain];
-
-        const marketplaces = existingMarketplaces.includes(marketplace)
-          ? existingMarketplaces
-          : [...existingMarketplaces, marketplace];
-
-        const updateExpression = `
-        SET owners = :owners,
-            totalVolume = :totalVolume,
-            totalVolumeUSD = :totalVolumeUSD,
-            dailyVolume = :dailyVolume,
-            dailyVolumeUSD = :dailyVolumeUSD,
-            floor = :floor,
-            floorUSD = :floorUSD,
-            marketCap = :marketCap,
-            marketCapUSD = :marketCapUSD,
-            updatedAt = :updatedAt,
-            chains = :chains,
-            marketplaces = :marketplaces`;
-
-        const expressionAttributeValues = {
-          ":owners": statistics.owners,
-          ":totalVolume": statistics.totalVolume,
-          ":totalVolumeUSD": statistics.totalVolumeUSD,
-          ":dailyVolume": statistics.dailyVolume,
-          ":dailyVolumeUSD": statistics.dailyVolumeUSD,
-          ":floor": statistics.floor,
-          ":floorUSD": statistics.floorUSD,
-          ":marketCap": statistics.marketCap,
-          ":marketCapUSD": statistics.marketCapUSD,
-          ":updatedAt": currentTime,
-          ":chains": chains,
-          ":marketplaces": marketplaces,
-        };
-
-        await dynamodb.transactWrite({
-          updateItems: [
-            {
-              Key: {
-                PK: `collection#${slug}`,
-                SK: "overview",
-              },
-              UpdateExpression: updateExpression,
-              ExpressionAttributeValues: expressionAttributeValues,
-            },
-            {
-              Key: {
-                PK: `collection#${slug}`,
-                SK: `chain#${chain}`,
-              },
-              UpdateExpression: updateExpression,
-              ExpressionAttributeValues: expressionAttributeValues,
-            },
-            {
-              Key: {
-                PK: `collection#${slug}`,
-                SK: `marketplace#${marketplace}`,
-              },
-              UpdateExpression: updateExpression,
-              ExpressionAttributeValues: expressionAttributeValues,
-            },
-          ],
+        await Collection.update({
+          slug,
+          chain,
+          marketplace,
+          statistics,
+          collection: existingCollection,
         });
       }
 
       // If collection doesn't exist, increment collection counts
       // and insert metadata and statistics
-      if (!existingCollection.length) {
-        await dynamodb.transactWrite({
-          updateItems: [
-            {
-              Key: {
-                PK: `collectionCount`,
-                SK: `chain#${chain}`,
-              },
-              UpdateExpression: "ADD collections :no",
-              ExpressionAttributeValues: {
-                ":no": 1,
-              },
-            },
-            {
-              Key: {
-                PK: `collectionCount`,
-                SK: `marketplace#${marketplace}`,
-              },
-              UpdateExpression: "ADD collections :no",
-              ExpressionAttributeValues: {
-                ":no": 1,
-              },
-            },
-          ],
-          putItems: [
-            {
-              PK: `collection#${slug}`,
-              SK: "overview",
-              category: "collections",
-              ...metadata,
-              ...statistics,
-              createdAt: currentTime,
-              updatedAt: currentTime,
-            },
-            {
-              PK: `collection#${slug}`,
-              SK: `chain#${chain}`,
-              category: `collections#chain#${chain}`,
-              ...metadata,
-              ...statistics,
-              createdAt: currentTime,
-              updatedAt: currentTime,
-            },
-            {
-              PK: `collection#${slug}`,
-              SK: `marketplace#${marketplace}`,
-              category: `collections#marketplace#${marketplace}`,
-              ...metadata,
-              ...statistics,
-              createdAt: currentTime,
-              updatedAt: currentTime,
-            },
-          ],
+      else {
+        await Collection.insert({
+          slug,
+          chain,
+          marketplace,
+          metadata,
+          statistics,
         });
       }
     } catch (e) {
